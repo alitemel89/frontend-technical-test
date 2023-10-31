73cefa202524ef7736d9895749eab5a6559628f0
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2023-10-31 12:31:52 +0300

    add canvas after clicking plus icon

---
+++ src/components/Charts.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+
+interface DatatypeScore {
+  id: string;
+  score: number;
+}
+
+interface Target {
+  id: string;
+  approvedSymbol: string;
+  approvedName: string;
+  datatypeScores?: DatatypeScore[]; // Make datatypeScores optional
+}
+
+interface ChartsProps {
+  target: Target;
+}
+
+const Charts = ({ target }: ChartsProps) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const chartRef = useRef<Chart | null>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const datatypeScores = target.datatypeScores;
+
+    // Destroy the existing Chart instance if it exists
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    // Extract scores and labels from datatypeScores
+    const labels = datatypeScores?.map((score) => score.id);
+    const scores = datatypeScores?.map((score) => score.score);
+
+    console.log(scores)
+
+    // Create a new chart with proper type definitions
+    const ctx = canvasRef.current;
+    chartRef.current = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: labels as string[], // Typecast to match expected type
+        datasets: [
+          {
+            label: "Association Scores",
+            data: scores as number[], // Typecast to match expected type
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }, [target.datatypeScores]);
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">
+        Association Scores for {target.approvedSymbol}
+      </h2>
+      <canvas ref={canvasRef} width="400" height="200"></canvas>
+    </div>
+  );
+};
+
+export default Charts;
--- src/components/OpenTargets.tsx
+++ src/components/OpenTargets.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
-import { Bar } from "react-chartjs-2";
+import { MinusCircleIcon, PlusCircleIcon } from "@heroicons/react/24/outline";
 import client from "../apollo";
 import { gql } from "@apollo/client";
 import { Link } from "react-router-dom";
+import Charts from "./Charts";
 
 interface Target {
   id: string;
@@ -50,6 +51,8 @@ const OPEN_TARGETS_QUERY = gql`
 `;
 
 const OpenTargets = () => {
+  const [expandedRows, setExpandedRows] = useState<Record<number, boolean>>({});
+
   const { loading, error, data } = useQuery<DiseaseData>(OPEN_TARGETS_QUERY, {
     client: client,
   });
@@ -59,14 +62,22 @@ const OpenTargets = () => {
 
   const targets = data?.disease.associatedTargets.rows;
 
+  const toggleRow = (rowIndex: number) => {
+    setExpandedRows({
+      ...expandedRows,
+      [rowIndex]: !expandedRows[rowIndex],
+    });
+  };
+
   return (
     <div className="px-12 mx-auto max-w-5xl">
-      <h1 className="text-2xl font-bold mb-4 text-center">
+      <h1 className="text-2xl font-bold mb-4 text-center mt-4">
         Top 10 Targets by Overall Association Score
       </h1>
       <table className="w-full border-collapse">
         <thead className="bg-emerald-500 text-white">
           <tr>
+            <th className="p-2 border"></th>
             <th className="p-2 border">Target Symbol</th>
             <th className="p-2 border">Overall Score</th>
             <th className="p-2 border">Approved Name</th>
@@ -74,20 +85,40 @@ const OpenTargets = () => {
         </thead>
         <tbody>
           {targets?.slice(0, 10).map((row, index) => (
-            <tr key={index}>
-              <td className="p-2 border text-center hover:text-emerald-600">
-                <Link
-                  to={`https://platform.opentargets.org/target/${row.target.id}/associations`}
+            <React.Fragment key={index}>
+              <tr>
+                <td
+                  className="p-2 border text-center hover:text-emerald-600 cursor-pointer"
+                  onClick={() => toggleRow(index)}
                 >
-                  {row.target.approvedSymbol}
-                </Link>
-              </td>
-
-              <td className="p-2 border text-center">{row.score.toFixed(4)}</td>
-              <td className="p-2 border text-center">
-                {row.target.approvedName}
-              </td>
-            </tr>
+                  {expandedRows[index] ? (
+                    <MinusCircleIcon className="h-6 w-6 inline-block mr-2 text-emerald-500" />
+                  ) : (
+                    <PlusCircleIcon className="h-6 w-6 inline-block mr-2 text-emerald-500" />
+                  )}
+                </td>
+                <td className="p-2 border text-center hover:text-emerald-600">
+                  <Link
+                    to={`https://platform.opentargets.org/target/${row.target.id}/associations`}
+                  >
+                    {row.target.approvedSymbol}
+                  </Link>
+                </td>
+                <td className="p-2 border text-center">
+                  {row.score.toFixed(4)}
+                </td>
+                <td className="p-2 border text-center">
+                  {row.target.approvedName}
+                </td>
+              </tr>
+              {expandedRows[index] && (
+                <tr>
+                  <td className="p-2 border" colSpan={4}>
+                    <Charts target={row.target} />
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
           ))}
         </tbody>
       </table>
