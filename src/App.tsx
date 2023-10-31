6729e5f96525e2653eee0e271e5a281c77cb6604
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2023-10-31 11:46:17 +0300

    display data table approvedSymbol, Gene Name and overaşş association score

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import OpenTargets from "./components/OpenTargets";
+
+function App() {
+  return (
+    <div>
+        <OpenTargets />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/OpenTargets.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { Bar } from "react-chartjs-2";
+import client from "../apollo";
+import { gql } from "@apollo/client";
+
+interface Target {
+  id: string;
+  approvedSymbol: string;
+  approvedName: string;
+}
+
+interface DatatypeScore {
+  id: string;
+  score: number;
+}
+
+interface Row {
+  target: Target;
+  score: number;
+  datatypeScores: DatatypeScore[];
+}
+
+interface AssociatedTargets {
+  rows: Row[];
+}
+
+interface DiseaseData {
+  disease: {
+    associatedTargets: AssociatedTargets;
+  };
+}
+
+const OPEN_TARGETS_QUERY = gql`
+  query lungCarcinomaAssociatedTargets {
+    disease(efoId: "EFO_0001071") {
+      associatedTargets(page: { index: 0, size: 10 }) {
+        rows {
+          target {
+            id
+            approvedSymbol
+            approvedName
+          }
+          score
+        }
+      }
+    }
+  }
+`;
+
+const OpenTargets = () => {
+  const { loading, error, data } = useQuery<DiseaseData>(OPEN_TARGETS_QUERY, {
+    client: client,
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const targets = data?.disease.associatedTargets.rows;
+
+  return (
+    <div className="px-12 mx-auto max-w-5xl">
+      <h1 className="text-2xl font-bold mb-4">
+        Top 10 Targets by Overall Association Score
+      </h1>
+      <table className="w-full border-collapse">
+        <thead className="bg-red-400 text-white">
+          <tr>
+            <th className="p-2 border">Target Symbol</th>
+            <th className="p-2 border">Overall Score</th>
+            <th className="p-2 border">Approved Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {targets?.slice(0, 10).map((row, index) => (
+            <tr key={index}>
+              <td className="p-2 border text-center">
+                {row.target.approvedSymbol}
+              </td>
+              <td className="p-2 border text-center">{row.score.toFixed(4)}</td>
+              <td className="p-2 border text-center">
+                {row.target.approvedName}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default OpenTargets;
